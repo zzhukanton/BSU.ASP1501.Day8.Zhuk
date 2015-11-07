2e7d49db8c41fc036c32772717b13802e439971a
zzhukanton <zzhukanton@gmail.com>
2015-11-08 01:56:02 +0200

    Added some tests

---
+++ Task1.Tests/LogicTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1.Logic;
+using Entities;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class LogicTests
+    {
+        [TestCase(new int[4] {1, 3, 5, 9}, new int[4] { 2, 5, 1, 8}, 2, Result = new int[4] {3, 6, 8, 17})]
+        [TestCase(null, null, 3, ExpectedException = typeof(ArgumentNullException))]
+        [TestCase(new int[1] {3}, new int[1] {6}, 1, Result = new int[1] {9})]
+        [TestCase(new int[1] { 3 }, new int[1] { 6 }, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(new int[5] {1, 2, 3, 4, 5}, new int[4] {1, 2, 3, 4}, 4, ExpectedException = typeof(ArgumentException))]
+        public int[] AddTest_SquareMatrix_Ints(int[] first, int[] second, int length)
+        {
+            SquareMatrix<int> firstMatrix = new SquareMatrix<int>(first, length);
+            SquareMatrix<int> secondMatrix = new SquareMatrix<int>(second, length);
+            int[] result = new int[length * length];
+
+            firstMatrix.CalculateOperation(secondMatrix, (a, b) => a + b);
+
+            int k = 0;
+            for (int i = 0; i < firstMatrix.Width; i++)
+                for (int j = 0; j < firstMatrix.Width; j++)
+                {
+                    result[k++] = firstMatrix[i, j];
+                }
+
+            return result;
+        }
+    }
+}
---
+++ Task1.Tests/MatrixTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Entities;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class MatrixTests
+    {
+        [TestCase(new int[4] {1, 3, 6, 9}, 2, 0, 0, Result = 1)]
+        [TestCase(new int[4] { 1, 3, 6, 9 }, 2, 1, 0, Result = 3)]
+        [TestCase(new int[4] { 1, 3, 6, 9 }, 2, 4, 4, ExpectedException = typeof(ArgumentException))]
+        [TestCase(new int[4] { 1, 3, 6, 9 }, 2, -1, 0, ExpectedException = typeof(ArgumentException))]
+        public int IndexatorTest_Int_SquareMatrix(int[] array, int length, int i, int j)
+        {
+            SquareMatrix<int> m = new SquareMatrix<int>(array, length);
+ 
+            return m[i, j];
+        }
+
+        [TestCase(new int[4] { 1, 3, 6, 9 }, 2, 2, Result = 6)]
+        [TestCase(new int[4] { 1, 3, 6, 9 }, 3, 0, Result = 0)]
+        [TestCase(new int[4] { 1, 3, 6, 9 }, 6, 4, ExpectedException = typeof(ArgumentException))]
+        [TestCase(new int[4] { 1, 3, 6, 9 }, -1, 0, ExpectedException = typeof(ArgumentException))]
+        public int IndexatorTest_Int_DiagonalMatrix(int[] array, int i, int j)
+        {
+            DiagonalMatrix<int> m = new DiagonalMatrix<int>(array);
+
+            return m[i, j];
+        }
+
+        [TestCase(new int[6] { 1, 3, 6, 9, 8, 1 }, 3, 2, 2, Result = 1)]
+        [TestCase(new int[4] { 1, 2, 3, 4}, 4, 1, 1, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 3, 1, 1, ExpectedException = typeof(ArgumentNullException))]
+        [TestCase(new int[6] {1, 2, 3, 4, 5, 6}, 4, 1, 1, ExpectedException = typeof(ArgumentException))]
+        [TestCase(new int[6] { 1, 3, 6, 9, 5, 2 }, 3, 7,  4, ExpectedException = typeof(ArgumentException))]
+        [TestCase(new int[6] { 1, 3, 6, 9, 8, 1 }, 3, -2, 0, ExpectedException = typeof(ArgumentException))]
+        public int IndexatorTest_Int_SymmetricalMatrix(int[] array, int length, int i, int j)
+        {
+            SymmetricalMatrix<int> m = new SymmetricalMatrix<int>(array, length);
+
+            return m[i, j];
+        }
+    }
+}
